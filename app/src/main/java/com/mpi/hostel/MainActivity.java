96df43a90b79e9b8f0b61ded67ec58531be7853d
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-08-22 20:18:17 +0600

    Hostel info added

--- app/src/main/java/com/mpi/hostel/MainActivity.java
+++ app/src/main/java/com/mpi/hostel/MainActivity.java
@@ -1,14 +1,38 @@
 package com.mpi.hostel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+//declaring global variables
+    CardView infopage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //calling init method
+
+        initall();
+
+
+        //sending to hostel info page
+        infopage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,HostelInfoActivity.class));
+            }
+        });
+
+    }
+
+    private void initall() {
+        infopage=findViewById(R.id.sentinfo);
+
     }
 }
