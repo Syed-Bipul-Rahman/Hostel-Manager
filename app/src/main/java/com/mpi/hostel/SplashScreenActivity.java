d5b1843029d69b5e054461b83513d5f49d5f0048
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-08-23 21:57:31 +0600

    Splash Activity added

---
+++ app/src/main/java/com/mpi/hostel/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.mpi.hostel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static final int SPLASH_DURATION = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+
+
+    }
+}
